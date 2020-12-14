f6466ce91106c72ba8a87316aadbf66f663879c4
emersonpessoa01 <eps011108@gmail.com>
2020-12-13 21:32:07 -0300

    inserted modal

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import TaskList from "./src/components/TaskLIst";
+import * as Animatable from "react-native-animatable"
+
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity)
 
 export default function App() {
+  const [task, setTask] = useState([
+    { key: 1, task: "Comprar pão" },
+    { key: 1, task: "Estudar React-Native" },
+    { key: 1, task: "Ir na academia hoje a noite" },
+    { key: 1, task: "Comprar Coca-cola" },
+    { key: 1, task: "Assistir ao vídeo do sujetio" },
+  ]);
+
+  const[open,setOpen] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#171431" barStyle="light-content " />
+      <View style={styles.content}>
+        <Text style={styles.title}>Minhas tarefas</Text>
+      </View>
+
+      <FlatList
+        marginHorizontal={10}
+        showsHorizontalScrollIndicator={false}
+        data={task}
+        keyExtractor={(item) => String(item.key)}
+        renderItem={({ item }) => <TaskList data={item} />}
+      />
+
+
+      <Modal animationType="slide" transparent={false} visible={open}>
+        <SafeAreaView>
+          <Text>Modal</Text>
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatedBtn 
+      style={styles.fab}
+      useNativeDriver
+      animation="bounceInUp"
+      duration={1500}
+      onPress={()=> setOpen(true)}
+
+      >
+        <Ionicons name="ios-add" size={35} color="#fff" />
+      </AnimatedBtn>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#171431",
+  },
+  title: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: "center",
+    color: "#fff",
+  },
+  fab: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    backgroundColor: "#0094ff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
   },
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+### App:
+* npm install expo-cli --global<br>
+* expo init <name_app><br>
+* expo start<br>
+* expo install react-native-animatable<br>
